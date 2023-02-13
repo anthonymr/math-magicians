9a8a18f27ecb06eea6015e1ea867302adaff678f
Anthony Martin <anthonymartin20@gmail.com>
2023-02-13 16:32:54 +0100

    Replace buttons tags by respective component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,5 @@
+import NumericButton from './NumericButton';
+import OperationsButton from './OperationButton';
 import './Calculator.css';
 
 const Calculator = () => (
@@ -8,30 +10,30 @@ const Calculator = () => (
     <div className="flexContainer">
       <div className="mainPanel">
         <div>
-          <button type="submit">AC</button>
-          <button type="submit">+/-</button>
-          <button type="submit">%</button>
+          <OperationsButton label="AC" />
+          <OperationsButton label="+/-" />
+          <OperationsButton label="%" />
         </div>
         <div>
-          <button type="submit">7</button>
-          <button type="submit">8</button>
-          <button type="submit">9</button>
-          <button type="submit">4</button>
-          <button type="submit">5</button>
-          <button type="submit">6</button>
-          <button type="submit">1</button>
-          <button type="submit">2</button>
-          <button type="submit">3</button>
-          <button type="submit" className="bigBtn">0</button>
-          <button type="submit">.</button>
+          <NumericButton digit={7} />
+          <NumericButton digit={8} />
+          <NumericButton digit={9} />
+          <NumericButton digit={4} />
+          <NumericButton digit={5} />
+          <NumericButton digit={6} />
+          <NumericButton digit={1} />
+          <NumericButton digit={2} />
+          <NumericButton digit={3} />
+          <NumericButton digit={0} className="bigBtn" />
+          <NumericButton digit="." />
         </div>
       </div>
       <div className="rightPanel">
-        <button type="submit">÷</button>
-        <button type="submit">x</button>
-        <button type="submit">-</button>
-        <button type="submit">+</button>
-        <button type="submit">=</button>
+        <OperationsButton label="÷" />
+        <OperationsButton label="x" />
+        <OperationsButton label="-" />
+        <OperationsButton label="+" />
+        <OperationsButton label="=" />
       </div>
     </div>
   </section>
